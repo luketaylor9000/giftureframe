58ef29a09bc800848257c0bbc70b66f9705674c3
luketaylor9000 <lukeandrewtaylor.lt@gmail.com>
2022-10-03 19:10:48 -0400

    implemented pyglet & save gifs locally

--- gifture.py
+++ gifture.py
@@ -1,45 +1,83 @@
-# to run type "python3 mojo_jojo.py" into cmd at the path where this file is
-#Will need to 
-#pip install pyglet
-#pip install dropbox
-#in cmd
-import dropbox
+from multiprocessing import Event
+import requests
+import json
+import argparse
+import random
+import os
+import urllib.request
 import pyglet
-import urllib
+import pyglet.window.key
 
-dbx = dropbox.Dropbox("dropbox key/code")
-mojojojolocalfilename = "mojo_jojo.gif"
-# from here we've signed into your dropbox account
-# the dropbox path for the root for this dropbox script is
-# Dropbox/Apps/Mojo_Jojo
-# Which is where Mojo Jojo gifs should be placed
 
-gif = dbx.files_get_temporary_link("/mojojojo1.gif")
-testfile = urllib.request.URLopener()
-testfile.retrieve(gif.link, mojojojolocalfilename)
+# set the apikey and limit
+apikey = ""  # click to set to your apikey
+lmt = 10    # set number of gifs to return
+ckey = ""  # set the client_key for the integration and use the same value for all API calls
 
-# the path for pyglet.resource.animation where you placed this python file
-sprite = pyglet.sprite.Sprite(pyglet.resource.animation(mojojojolocalfilename))
+search_term = "excited"  # set a search term
 
-H_ratio = max(sprite.height, 720) / min(sprite.height, 720)
-W_ratio = max(sprite.width, 720) / min(sprite.width, 720)
+r = requests.get(
+    "https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, apikey, ckey,  lmt))
 
-sprite.scale = min(H_ratio, W_ratio)
+gif_list = []
 
-window = pyglet.window.Window(width=720, height=720, fullscreen=True)
+def dl_gif(url, file_path, file_name):
+    full_path = file_path + file_name + '.gif'
+    urllib.request.urlretrieve(url, full_path)
 
-def close(event):
-    win.close()
+if r.status_code == 200:
+    gif_json_data = json.loads(r.content)
+    for each in gif_json_data['results']:
+        gif = (each['media_formats']['gif']['url'])
+        gif_description = (each['content_description'])
+        url = gif
+        index = gif_json_data['results'].index(each)
+        file_name = (f'gif{index}')
+
+        dl_gif(url, 'images/', file_name)   
+else:
+    gif_json_data = None
+
+
+def update_image(dt):
+    ani = pyglet.resource.animation(random.choice(image_paths))
+    sprite.image = ani
+    sprite.scale_x = max(sprite.height, 720) / min(sprite.height, 720)
+    sprite.scale_y = max(sprite.width, 720) / min(sprite.width, 720)
+    window.clear()
+
+
+def get_image_paths(input_dir='.'):
+    paths = []
+    for root, dirs, files in os.walk(input_dir, topdown=True):
+        for file in sorted(files):
+            if file.endswith(('gif')):
+                path = f'images/{file}'
+                paths.append(path)
+    return paths
+
+
+window = pyglet.window.Window(width=720, height=720)
+# window = pyglet.window.Window(width=720, height=720, fullscreen=True)
 
 @window.event
 def on_draw():
     window.clear()
     sprite.draw()
 
-pyglet.app.run()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dir', help='directory of images', nargs='?', default=os.getcwd())
+    args = parser.parse_args()
 
-##Things to expand on
+    image_paths = get_image_paths(args.dir)
+    ani = pyglet.resource.animation(random.choice(image_paths))
 
-##Dynamic resolution scaling
+    sprite = pyglet.sprite.Sprite(ani)
+    H_ratio = max(sprite.height, 720) / min(sprite.height, 720)
+    W_ratio = max(sprite.width, 720) / min(sprite.width, 720)
+    sprite.scale = min(H_ratio, W_ratio)
 
-##Pull multiple gifs instead of a single one
+    pyglet.clock.schedule_interval(update_image, 6.0)
+
+    pyglet.app.run()
